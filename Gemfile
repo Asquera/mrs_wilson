3bde8734c622296354c6d338bb9e63038c3c10b1
Florian Gilcher <florian.gilcher@asquera.de>
2011-07-09 08:17:32 +0200

    Hacky timer support using harvested

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source :rubygems
 
+gem 'hashie'
 gem 'blather'
 gem 'state_machine'
+gem 'harvested', :git => 'https://github.com/Asquera/harvested.git'
+
+gem 'riot', :group => 'test'
--- lib/mrs_wilson.rb
+++ lib/mrs_wilson.rb
@@ -1,48 +1,14 @@
 require 'rubygems'
 require 'blather/client/dsl'
-require "state_machine"
-
-class Harvest
-  attr_accessor :wilson
-  
-  def initialize(wilson)
-    self.wilson = wilson
-    super()
-  end
-    
-  state_machine :initial => :stopped do
-    event :start do
-      transition :started => same, :stopped => :started
-    end
-    
-    event :stop do
-      transition :stopped => same, :started => :stopped
-    end
-    
-    before_transition :stopped => :start, :do => :log_transition
-    
-    event :start_choosing do
-      transition any => :choosing
-    end
-    
-    around_transition do |obj, transition, block|
-      if transition.to_name == :choosing
-        EM.add_timer(3) do
-          transition.rollback
-          MrsWilson.say_to_master("You didn't choose quick enough, rolling back")
-        end
-        
-      end
-      block.call
-    end
-  end
-end
+require 'state_machine'
+require 'harvested'
 
 module MrsWilson
   extend Blather::DSL
   
   def self.harvest
-    @harvest ||= Harvest.new(self)
+    #@harvest ||= Harvest.new(self)
+    @harvest ||= Harvest.hardy_client('asquera', 'florian.gilcher@asquera.de', 'Yooqu4ve')
   end
   
   def self.master(master = nil)
@@ -77,15 +43,14 @@ module MrsWilson
     
   end
   
-  
   message :chat?, :body => 'exit' do |m|
     say_to_master 'Exiting ...'
     shutdown
   end
   
-  message :chat?, :body => "Who are you?" do |m|
-    say m.from, "What gift do you think a good servant has that separates them from the others? Its the gift of anticipation. And I'm a good servant; I'm better than good, I'm the best; I'm the perfect servant. I know when they'll be hungry, and the food is ready. I know when they'll be tired, and the bed is turned down. I know it before they know it themselves."
-  end
+  #message :chat?, :body => "Who are you?" do |m|
+  #  say m.from, "What gift do you think a good servant has that separates them from the others? Its the gift of anticipation. And I'm a good servant; I'm better than good, I'm the best; I'm the perfect servant. I know when they'll be hungry, and the food is ready. I know when they'll be tired, and the bed is turned down. I know it before they know it themselves."
+  #end
   
   message :chat?, :body => /#c/ do |m|
     harvest.start_choosing
@@ -97,10 +62,18 @@ module MrsWilson
     say m.from, "You said: #{text}"
   end
   
+  message :chat?, :body => /#ts.*/ do |m|
+    timer = stop_timer
+    
+    say m.from, "Stopped timer #{timer.notes}"
+  end
+  
   message :chat?, :body => /#t (.*)/ do |m|
     text = /#t(.*)/.match(m.body)[1]
-    track(text)
-    say m.from, "Tracking your time: #{$1}"
+    timer = track(text)
+    
+    say m.from, "Started timer #{timer.id}: #{timer.notes}"
+    say m.from, "Last timer was running for: #{timer.hours_for_previously_running_timer}" if timer.hours_for_previously_running_timer
   end
   
   def self.run
@@ -108,7 +81,12 @@ module MrsWilson
   end
   
   def self.track(text)
-    harvest.start!
+    harvest.time.create(notes: text, project_id: 781990, task_id: 461119)
+  end
+  
+  def self.stop_timer
+    timer = harvest.time.all.last
+    harvest.time.toggle(timer)
   end
   
   disconnected { client.connect }
